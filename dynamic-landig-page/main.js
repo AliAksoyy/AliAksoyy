d64f7f8ebc531729778c7d6eaddb5a29868ce5f3
Ali <alifeyza4017@gmail.com>
2022-08-29 08:49:05 +0300

    added

---
+++ dynamic-landig-page/main.js
@@ -0,0 +1,8 @@
+// DOM Elements
+
+const time = document.getElementById('time'),
+greeting = document.getElementById('greeting'),
+name = document.getElementById('name'),
+focus = document.getElementById('focus')
+
+//
