2d0064c7d9470ce5bda8db992a7a5cb92f5305a3
Ali <alifeyza4017@gmail.com>
2022-09-01 14:54:54 +0300

    added

---
+++ dynamic-landig-page/README.md
@@ -0,0 +1,116 @@
+# js_landing_page
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Skeleton](#skeleton)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See different background images at different times of the day
+- not have to reenter their names and focuses when the page is refreshed.
+
+### the purpose
+
+-creating a dynamic landing page.
+
+### Screenshot
+
+<p align="center">
+<a href="https://erkan-y.github.io/js_landing_page/"><img src="desktop.gif" alt="screenshot" width="750" height="550"></a>
+</p>
+<p align="center">
+<a href="https://erkan-y.github.io/js_landing_page/"><img src="mobile.gif" alt="screenshot" width="750" height="550"></a>
+</p>
+
+
+### Links
+
+- [live site URL here: ([Live Website)](https://erkan-y.github.io/js_landing_page/)
+
+## Project Skeleton 
+
+```
+# js_landing_page(folder)
+
+|----README.md                   
+|----img      
+     | photos...
+|----index.html  
+|----style.css   
+|--- main.js
+
+```
+
+## My process
+
+### Built with
+- JS DOM
+- JS Functions
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+- [Styled Components](https://styled-components.com/) - For styles
+
+
+### What I learned
+
+I learned how to use Javascript DOM Manipulation, flexboxes, media queries, html elements and many more.
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  @media only screen and (min-width: 790px) {
+}
+```
+```js
+        if(e.which ==13 || e.keyCode == 13) {
+            localStorage.setItem("names", e.target.innerText);
+            names.blur();
+        }else {
+            localStorage.setItem("names", e.target.innerText);
+        }
+}
+```
+
+
+### Continued development
+
+I will build backend and extra pages for this website in the future.
+
+**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
+
+### Useful resources
+
+- [W3 Schoold](https://www.w3schools.com/) - This helped me for basics of website paradigm. I really liked this pattern and will use it going forward.
+- [MDN](https://developer.mozilla.org/en-US/) - This is an amazing document which helped me finally understand deep sides of web development. I'd recommend it to anyone still learning these concepts.
+
+
+## Author
+
+- Author - [Erkan]
--- dynamic-landig-page/main.js
+++ dynamic-landig-page/main.js
@@ -1,125 +1,116 @@
-// DOM Elements
+// * DOM Elements
 
-const time = document.getElementById('time'),
-greeting = document.getElementById('greeting'),
-name = document.getElementById('name'),
-focus = document.getElementById('focus');
-
-//Options
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+//* Options
 const showAmPm = true;
 
-//Show Time
+//* Show Time
 
 function showTime() {
-    let today =new Date(),
+  let today = new Date(),
+    //   let today = new Date(2022, 06, 10, 8, 33, 30),
     hour = today.getHours(),
     min = today.getMinutes(),
-    sec = today.getSeconds()
-    // Set AM or PM
+    sec = today.getSeconds();
 
-    
-    const amPm = hour>=12 ? "PM" : "AM";
+  //* Set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
 
-    //12hr Format
+  //* 12 hr format
+  hour = hour % 12 || 12;
 
-    hour = hour % 12 || 12;
+  //* Output time
+  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${addZero(sec)} ${
+    showAmPm ? amPm : ""
+  }`;
 
-    // Output Time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
-
-    setTimeout(showTime,1000);
-   
+  setTimeout(showTime, 1000);
 }
-
- // Add Zero
-    function addZero(n) {
-        return (parseInt(n,10)< 10 ? "0" : "") + n;
-    }
-// Set background and Greeting
+//* Add Zeros (sec shows only "0". we have to change it!)
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+//* Set background and Greeting
 function setBgGreet() {
-    let today = new Date(),
-    hour = today.getHours()
+  let today = new Date(),
+    //   let today = new Date(2022, 06, 10, 8, 33, 30),
+    hour = today.getHours();
 
-    if(hour<12) {
-        // Morning
-        document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundPosition = "center"
-        document.body.style.backgroundSize = "center";
-        greeting.textContent = 'Good Morning'
-
-    } else if(hour<18) {
-        //Afternoon
-        document.body.style.backgroundImage = "url('../img/evening.jpg')";
-        greeting.textContent = "Good Morning";
-    }else {
-        //Evening
-        document.body.style.backgroundImage = "url('../img/nights.jpg')";
-        greeting.textContent = "Good Morning";
-        document.body.style.color = 'white'
-    }
-
+  if (hour < 12) {
+    //?Morning
+    document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+    greeting.textContent = "Good Morning";
+    document.body.style.color = "white";
+  } else if (hour < 16) {
+    document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+    document.body.style.color = "white";
+  } else if (hour < 18) {
+    //? Afternoon
+    document.body.style.backgroundImage = "url('./img/evening.jpg')";
+    greeting.textContent = "Good Afternoon";
+    document.body.style.color = "white";
+  } else {
+    //? Evening
+    document.body.style.backgroundImage = "url('./img/nights.jpg')";
+    greeting.textContent = "Good night";
+    document.body.style.color = "white";
+  }
 }
-// Get Focus
 
-
-
-
-// Get Name
-
+//* Get Name
 function getName() {
-    if(localStorage.getItem('name') === null) {
-        name.textContent = '[Enter Name]';
-    } else {
-        name.textContent = localStorage.getItem('name')
-    }
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "Enter Name";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
 }
-
-// Set Name
-
+//* Set Name
 function setName(e) {
-
-    if(e.type === 'keypress') {
-        // Make sure enter is pressed
-        if(e.which == 13 || e.keyCode == 13) {
-           localStorage.setItem("name", e.target.innerText); 
-            name.blur()
-        }
-    } else {
-       localStorage.setItem('name', e.target.innerText) 
+  if (e.type === "keypress") {
+    //? make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
     }
-}
-// Set Focus
-
-function setFocus(e) {
-
-    if(e.type === 'keypress') {
-        // Make sure enter is pressed
-        if(e.which == 13 || e.keyCode == 13) {
-           localStorage.setItem("focus", e.target.innerText); 
-            name.blur()
-        }
-    } else {
-       localStorage.setItem('focus', e.target.innerText) 
-    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
 }
 
-// Get focus
+//* Get Focus
 function getFocus() {
-    if(localStorage.getItem('focus') === null) {
-        focus.textContent = '[Enter Focus]';
-    } else {
-        focus.textContent = localStorage.getItem('focus')
-    }
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "Enter Focus";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
 }
 
-name.addEventListener('keypress',setName)
-name.addEventListener('blur',setName)
+//* Set Focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    //? make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
 focus.addEventListener("keypress", setFocus);
 focus.addEventListener("blur", setFocus);
-
- // run
-    showTime();
-    setBgGreet();
-    getName();
-    getFocus();
+//* Run
+showTime();
+setBgGreet();
+getName();
+getFocus();
+setName();
